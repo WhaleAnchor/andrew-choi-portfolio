d775a6603c5fd8fea7cb68594463943485654024
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-23 14:32:52 -0800

    Updated app.js to declutter and make home.js more streamlined

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Navbar from './Navbar';
 import Home from './Home';
-import Projects from './Projects';
-import About from './About';
-import Contact from './Contact';
+import NotFound from './NotFound';
 
-function Layout() {
-  const [selected, setSelected] = useState('home');
-  const location = useLocation();
-
-  useEffect(() => {
-    // Update the selected item in the navbar when the URL changes
-    const path = location.pathname.substring(1);
-    setSelected(path || 'home');
-  }, [location]);
-
+function App() {
   return (
-    <div className="layout">
-      <Navbar selected={selected} setSelected={setSelected} />
-      <main className="main">
-        <Switch>
-          <Route path="/home" component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-          <Route component={Home} />
-        </Switch>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-export default Layout;
+export default App;
