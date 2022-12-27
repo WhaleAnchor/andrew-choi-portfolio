77f3c32f6b1145d8931b2641aae54f30470914b5
Andrew Choi <andrewsungwoochoi@gmail.com>
2022-12-26 19:29:16 -0800

    Updated Home.js to contain new components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
-import Navbar from './Navbar';
-import Projects from './Projects';
-import About from './About';
-import Contact from './Contact';
+
+import Navbar from '../components/Navbar/Navbar';
+import Projects from '../components/Projects/Projects';
+import About from '../components/About/About';
+import Contact from '../components/Contact/Contact';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+}));
 
 function Home() {
-  const [selected, setSelected] = useState('projects');
-  const location = useLocation();
-
-  useEffect(() => {
-    // Update the selected item in the navbar when the URL hash changes
-    const hash = location.hash.substring(1);
-    setSelected(hash || 'projects');
-  }, [location]);
+  const classes = useStyles();
 
   return (
-    <div className="home">
-      <Navbar selected={selected} setSelected={setSelected} />
-      <main className="main">
-        <Switch>
-          <Route path="#projects" component={Projects} />
-          <Route path="#about" component={About} />
-          <Route path="#contact" component={Contact} />
-          <Route component={Projects} />
-        </Switch>
-      </main>
-    </div>
+    <>
+      <div className={classes.root}>
+        <Navbar/>
+        <main className="main">
+          <>
+            <About/>
+            <Projects/>
+            <Contact/>
+          </>
+        </main>
+      </div>
+    </>
   );
 }
 
